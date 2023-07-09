a8225b6c1ede40c227d689ccfef6a3261f14d17b
Kalem Schrock <kalemschrock@gmail.com>
2023-07-09 10:51:30 +0000

    auto-deployment update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for, flash, redirect, url_for
+from flask import Flask, render_template, url_for, flash, redirect, url_for, request
 from forms import RegistrationForm
 from flask_behind_proxy import FlaskBehindProxy
+import git
 app = Flask(__name__)                    
 # this gets the name of the file so Flask knows it's name
 proxied = FlaskBehindProxy(app)  ## add this line
@@ -25,6 +26,15 @@ def register():
         return redirect(url_for('home')) # if so - send to home page
     return render_template('register.html', title='Register', form=form)
 
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/CHANGE_TO_PYTHON_ANYWHERE_USERNAME/CHANGE_TO_GITHUB_REPO_NAME')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 if __name__ == '__main__':               
   # this should always be at the end
